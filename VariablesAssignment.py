5f4e66b39b9bdaffbd0250528a875ca2291f8e4b
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2021-08-24 09:16:05 +0530

    Add files via upload

---
+++ VariablesAssignment.py
@@ -0,0 +1,22 @@
+"""
+My Favourite Song and its meta-data
+Storing all the commented details and printing them one below the other
+"""
+Artist = "OneRepublic" #Artist of the song
+Genre = "Pop" #Genre of the song
+DurationInSeconds = 169 #Duration of the song in seconds
+YearOfRelease = 2021 #Release date of the song
+RecordLabel = "Universal Music Group" #License of the song attributed to
+LikeToDislikeRatio = 95.18 #Ratio of the likes and dislikes on YouTube platform
+Views =  6410185 # Views on YouTube platform
+Album = "Human" #Album of the Song
+LeadVocalist = "Ryan Tedder" #Lead Vocalist of the Song
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+print(YearOfRelease)
+print(RecordLabel)
+print(LikeToDislikeRatio)
+print(Views)
+print(Album)
+print(LeadVocalist)
