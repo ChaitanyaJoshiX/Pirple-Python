9afe1a5e21ff8b342f81cbdc7e4574b450d3ba26
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2021-08-26 16:15:46 +0530

    Add files via upload

---
+++ FunctionsAssignment.py
@@ -0,0 +1,20 @@
+"""
+Assigning attributes of my favourite song in a function
+Calling the function and displaying them one below the other
+"""
+def RecordLabel() : #Record Label of the song
+    Output = "Universal Music Group"
+    return Output
+def DurationInSeconds() : #Duration of the song
+    Output = 169
+    return Output
+def Album() : #Album which contains the song
+    Output = "Human"
+    return Output
+def HaveMillionsViewed() : #Does the song have a million views on YouTube platform at least
+    Output = True
+    return Output
+print(RecordLabel())
+print(DurationInSeconds())
+print(Album())
+print(HaveMillionsViewed())
